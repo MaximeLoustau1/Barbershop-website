12a6cd7420f9bb785a8634deb21c40289869b283
MaximeLoustau1 <122365264+MaximeLoustau1@users.noreply.github.com>
2024-05-20 14:28:50 +0200

    fixed: navbar/about/contact

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -10,6 +10,13 @@ function Navbar() {
         setIsMenuOpen(!isMenuOpen);
     };
 
+    function hideSideBar() {
+        const navbarContainer = document.querySelector('.navbar-container');
+        navbarContainer.classList.remove('show');
+        const hamburgerICon = document.querySelector('.menu-icon');
+        hamburgerICon.classList.remove('open');
+    }
+
     return(
         <section id="navbar-section">
             <div className={isMenuOpen ? 'menu-icon open' : 'menu-icon'} onClick={handleMenuClick}>
@@ -19,10 +26,10 @@ function Navbar() {
             </div>
             <div className={isMenuOpen ? 'show navbar-container' : 'navbar-container'}>
                 <div className="navbar-menu">
-                    <div className="navbar-menu-item"><a href='#about-section'>About</a></div>
-                    <div className="navbar-menu-item prices-item"><a href='#services-section'>Prices</a></div>
-                    <div className="navbar-menu-item"><a href='#team-section'>Team</a></div>
-                    <div className="navbar-menu-item"><a href='#schedule-section'>Contact</a></div>
+                    <div className="navbar-menu-item" onClick={hideSideBar}><a href='#about-section'>About</a></div>
+                    <div className="navbar-menu-item prices-item" onClick={hideSideBar}><a href='#services-section'>Prices</a></div>
+                    <div className="navbar-menu-item" onClick={hideSideBar}><a href='#team-section'>Team</a></div>
+                    <div className="navbar-menu-item" onClick={hideSideBar}><a href='#schedule-section'>Contact</a></div>
                 </div>
 
                 <div className="navbar-cta">
